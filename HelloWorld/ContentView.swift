f088b76b2962ea0da7e81533b9b15944a2a410eb
Mohammed <m7md21999@gmail.com>
2020-06-06 20:43:04 +0300

    added hello world project

---
+++ HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by Mohammed Aldawsari on 6/5/20.
+//  Copyright © 2020 Mohammed Aldawsari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
